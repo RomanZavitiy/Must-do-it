04bf349163f26938f81833ddee600a472c447d3a
White Wolf <syniak.int@gmail.com>
2019-05-03 21:57:29 +0200

    auth page 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Must do it
+
+A PhoneGap app
---
+++ www/js/index.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBuFYodHPQNjoV6XcsxL2LX10xJG17D1ZQ",
+    authDomain: "must-do-it.firebaseapp.com",
+    databaseURL: "https://must-do-it.firebaseio.com",
+    projectId: "must-do-it",
+    storageBucket: "must-do-it.appspot.com",
+    messagingSenderId: "392509090934"
+  };
+  firebase.initializeApp(config);
